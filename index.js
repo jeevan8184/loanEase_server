0bfca9b72676da0f5ca67f8421b6aa86f73c187e
jeevan8184 <jeevanjatavath7@gmail.com>
2024-10-20 23:38:47 +0530

    first commit

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
-
 import http from "http";
 import mongoose, { mongo } from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import bodyParser from "body-parser";
-import authRouter from "./routes/auth.route.js"
-import loanRouter from "./routes/loan.route.js"
+import authRouter from "./routes/auth.route.js";
+import loanRouter from "./routes/loan.route.js";
 
 dotenv.config();
-const app=express();
+const app = express();
 
-app.use(bodyParser.json({limit:"50mb",extended:true}));
-app.use(bodyParser.urlencoded({limit:"50mb",extended:true}));
-app.use(cors({ origin: '*' }));
+app.use(bodyParser.json({ limit: "50mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+const corsOpts = {
+  origin: "*",
 
-app.use("/auth",authRouter);
-app.use("/loan",loanRouter);
+  methods: ["GET", "POST"],
 
-app.use("/",(req,res)=> {
-    res.send("Hello Guys its not working");
-})
+  allowedHeaders: ["Content-Type"],
+};
+app.use(cors(corsOpts));
 
-mongoose.connect(process.env.MONGODB_URI,{dbName:"assign"}).catch((err) => console.log(err)).then(()=> console.log("mongodb connected"));
+app.use("/auth", authRouter);
+app.use("/loan", loanRouter);
 
-app.listen(process.env.PORT,()=> console.log(`Server is running on port ${process.env.PORT}`));
+app.use("/", (req, res) => {
+  res.send("Hello Guys its not working");
+});
 
+mongoose
+  .connect(process.env.MONGODB_URI, { dbName: "assign" })
+  .catch((err) => console.log(err))
+  .then(() => console.log("mongodb connected"));
 
+app.listen(process.env.PORT, () =>
+  console.log(`Server is running on port ${process.env.PORT}`)
+);
