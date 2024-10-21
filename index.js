df4afad988a7f4310eae57348209c09ce6994fe6
jeevan8184 <jeevanjatavath7@gmail.com>
2024-10-21 09:05:03 +0530

    first commit

--- index.js
+++ index.js
@@ -13,9 +13,14 @@ const app = express();
 app.use(bodyParser.json({ limit: "50mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
-app.use(cors({
-  origin:"http://localhost:3000",
-}));
+app.use(cors());
+app.use(
+  cors({
+    origin: "https://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.use("/auth", authRouter);
 app.use("/loan", loanRouter);
