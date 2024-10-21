ff710ee5fd3c0235d3ceaab7388555ff751e5c14
jeevan8184 <jeevanjatavath7@gmail.com>
2024-10-21 18:53:20 +0530

    first commit

--- index.js
+++ index.js
@@ -14,20 +14,20 @@ app.use(bodyParser.json({ limit: "50mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 const corsOptions = {
-  origin: "http://localhost:3000", // Use "http" if you are testing locally
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
 
-app.use(cors(corsOptions)); // Apply CORS with the specific configuration
+app.use(cors(corsOptions));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // Match origin to CORS options
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   
   if (req.method === "OPTIONS") {
-    return res.sendStatus(204); // Respond OK for pre-flight check
+    return res.sendStatus(204);
   }
   next();
 });
