c5d626dd4d479adf92a533b1f322ea1f48f895d8
jeevan8184 <jeevanjatavath7@gmail.com>
2024-10-21 18:38:57 +0530

    first comit

--- index.js
+++ index.js
@@ -10,33 +10,30 @@ import cors from "cors";
 dotenv.config();
 const app = express();
 
-// Body Parser configuration
 app.use(bodyParser.json({ limit: "50mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
-// CORS middleware setup
-app.use(cors());
-app.use(
-  cors({
-    origin: "https://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: "http://localhost:3000", // Use "http" if you are testing locally
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions)); // Apply CORS with the specific configuration
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://localhost:3000"); // or '*'
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // Match origin to CORS options
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   
   if (req.method === "OPTIONS") {
-    return res.status(200).json({});
+    return res.sendStatus(204); // Respond OK for pre-flight check
   }
   next();
 });
+
 app.use("/auth", authRouter);
 app.use("/loan", loanRouter);
-
 app.use("/", (req, res) => {
   res.send("Hello Guys, it's not working");
 });
@@ -45,6 +42,7 @@ mongoose
   .connect(process.env.MONGODB_URI, { dbName: "assign" })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
+
 app.listen(process.env.PORT, () =>
   console.log(`Server is running on port ${process.env.PORT}`)
 );
